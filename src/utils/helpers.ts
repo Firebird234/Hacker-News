89febe4f3ea7c1288d4467ad5fff7719446ef1d8
dzirrt01 <dzirrt01@yandex.ru>
2022-08-24 15:46:50 +0300

    Revert "main final version"
    This reverts commit e2a2e927ae02ef1d6c9c0f16279fcde071ff7c38.

--- src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { IError } from '../types/types';
-
-export function padTo2Digits(num: any) {
-  return num.toString().padStart(2, '0');
-}
-
-export function formatDate(date: any) {
-  return [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join('/');
-}
